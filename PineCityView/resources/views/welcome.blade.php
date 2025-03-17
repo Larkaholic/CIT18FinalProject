c73ab686ea59f12829750e584be9818d9b1d5b4f
Mr.KyDev <144975472+MrKyDev@users.noreply.github.com>
2025-03-17 15:20:56 +0800

    Set Up

---
+++ PineCityView/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="../public/img/Favicon.png" type="image/x-icon"/>
+        <title>Pine City View</title>   
+        
+    </head>
+    <header class="w-full h-screen bg-cover bg-center" style="background-image: url('../public/img/header-back.jpg');">
+    </header>
+    <body >
+                
+    </body>
+</html>
