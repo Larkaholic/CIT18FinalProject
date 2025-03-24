3bd64e36a28653c55859a29a4957f4ccf207d246
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-03-25 06:24:10 +0800

    Change welcome message for logged in user

--- finalProject/resources/views/dashboard.blade.php
+++ finalProject/resources/views/dashboard.blade.php
@@ -9,7 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{-- {{ __("You're logged in!") }} --}}
+                    <p>Welcome to MovieVault, {{ auth()->user()->name }}!</p>
+                    <br>
+                    <p>Thank you for joining our MovieVault Community! Our website is currently in development. Check back later!</p>
                 </div>
             </div>
         </div>
