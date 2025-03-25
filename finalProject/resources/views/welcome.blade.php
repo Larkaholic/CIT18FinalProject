621e3171e748ff290e0eaf4db6e48dcbf1464c81
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-03-25 20:57:17 +0800

    Clean unnecessary code

--- finalProject/resources/views/welcome.blade.php
+++ finalProject/resources/views/welcome.blade.php
@@ -11,11 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @else
-            <style></style>
-        @endif
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
