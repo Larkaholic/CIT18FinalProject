4ba06cda11ca1c975f7c9f5d205cc59fba449e43
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-03-27 04:06:00 +0800

    Add Movie model attributes

--- finalProject/app/Models/Movie.php
+++ finalProject/app/Models/Movie.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Movie extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'release_date',
+        'genre',
+        'poster_path',
+        'director',
+        'cast',
+        'average_rating',
+    ];
+
+    protected $casts = [
+        'release_date' => 'date',
+        'average_rating' => 'float',
+    ];
 }
