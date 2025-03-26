e25862c6c09c48830489282a87e53bedadd07b55
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-03-27 04:01:51 +0800

    Add MovieController

---
+++ finalProject/app/Http/Controllers/MovieController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class MovieController extends Controller
+{
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+
+        $movies = Movie::where('title', 'like', "%$query%")
+            ->orWhere('description', 'like', "%$query%")
+            ->get();
+
+        return view('search_results', compact('movies', 'query'));
+    }
+}
