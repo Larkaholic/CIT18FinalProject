01722da675e46c5858215b878dae934491b1a81b
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-03-27 04:49:10 +0800

    Adjust search bar

--- finalProject/resources/views/search.blade.php
+++ finalProject/resources/views/search.blade.php
@@ -33,9 +33,9 @@
                 type="text"
                 name="query"
                 placeholder="Search for movies..."
-                class="text-sm md:text-md w-2/3 lg:w-full md:w-2/3 sm:w-2/3 md:p-4 rounded-md shadow-md focus:ring focus:ring-gray-900 dark:text-gray-800 dark:bg-gray-200"
+                class="text-sm md:text-lg w-full p-3 rounded-md shadow-md focus:ring focus:ring-gray-900 dark:text-gray-800 dark:bg-gray-200"
             />
-            <button type="submit" class="absolute right-4 top-4">
+            <button type="submit" class="absolute right-4 top-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                 </svg>
