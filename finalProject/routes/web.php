7f728b3196e65c05dc0ef380358d6a5318923f8e
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-03-27 05:24:45 +0800

    Update search view

--- finalProject/routes/web.php
+++ finalProject/routes/web.php
@@ -22,6 +22,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/home', function () {
+        return view('welcome');
+    });
+    
+    Route::get('/search', [MovieController::class, 'search'])->name('search');
+    
+    Route::get('/search', [MovieController::class, 'search'])->name('search');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
