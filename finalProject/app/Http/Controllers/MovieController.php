9c28024eb0f9c805858754f643ba752cabcddf76
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-03-27 13:53:46 +0800

    Add showGenre function

--- finalProject/app/Http/Controllers/MovieController.php
+++ finalProject/app/Http/Controllers/MovieController.php
@@ -17,4 +17,18 @@ class MovieController extends Controller
 
         return view('search', compact('movies', 'query'));
     }
+
+    public function showGenre(Request $request, $genre = null)
+    {
+        $genres = Movie::pluck('genre')->flatMap(function ($genres) {
+            return explode(', ', $genres);
+        })->unique()->toArray();
+
+        if ($genre) {
+            $movies = Movie::where('genre', 'like', '%' . $genre . '%')->get();
+            return view('genres', compact('genres', 'movies', 'genre'));
+        } else {
+            return view('genres', compact('genres'));
+        }
+    }
 }
