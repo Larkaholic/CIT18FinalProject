cf8eb8d844efabb74d3bc2f2b98924422dea037d
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-03-27 17:42:01 +0800

    Add watchlist button

--- finalProject/resources/views/movie_details.blade.php
+++ finalProject/resources/views/movie_details.blade.php
@@ -10,23 +10,44 @@
             <div class="md:col-span-1">
                 <h1 class="text-2xl font-semibold mb-4 dark:text-white">{{ $movie->title }}</h1>
 
-                {{-- Favorite Button --}}
-                <form action="{{ route('favorite', $movie->id) }}" method="POST">
-                    @csrf
-                    <button type="submit" class="flex items-center p-2 bg-red-500 hover:bg-red-700 text-white rounded-md">
-                        @if ($isFavorite)
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
-                            </svg>
-                            Remove from Favorites
-                        @else
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                            </svg>
-                            Add to Favorites
-                        @endif
-                    </button>
-                </form>
+                {{-- User Interactions --}}
+                <div class="mt-4 flex gap-4">
+                    {{-- Favorite Button --}}
+                    <form action="{{ route('favorite', $movie->id) }}" method="POST">
+                        @csrf
+                        <button type="submit" class="flex items-center p-2 bg-red-500 hover:bg-red-700 text-white rounded-md">
+                            @if ($isFavorite)
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
+                                </svg>
+                                Remove from Favorites
+                            @else
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                </svg>
+                                Add to Favorites
+                            @endif
+                        </button>
+                    </form>
+
+                    {{-- Watchlist Button --}}
+                    <form action="{{ route('watchlist', $movie->id) }}" method="POST">
+                        @csrf
+                        <button type="submit" class="flex items-center p-2 text-white hover:text-red-500 rounded-md">
+                            @if ($isWatchlist)
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="red">
+                                    <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                                </svg>
+                                In Watchlist
+                            @else
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                Add to Watchlist
+                            @endif
+                        </button>
+                    </form>
+                </div>
 
                 <div class="mb-4 mt-4">
                     <h2 class="text-lg font-semibold dark:text-white">Description</h2>
