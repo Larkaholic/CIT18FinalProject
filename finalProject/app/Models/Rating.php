4c4857808db0cd302188cc17dd7c0b596e4a6f3e
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2025-03-27 17:47:36 +0800

    Add functions and attributes for rating model

--- finalProject/app/Models/Rating.php
+++ finalProject/app/Models/Rating.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Rating extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'movie_id',
+        'rating',
+        'review',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class);
+    }
 }
